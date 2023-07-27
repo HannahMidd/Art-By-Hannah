4e783e5c971883a7ecfb2092ba8c923b7b4688f3
HannahMidd <hmiddleton1017@gmail.com>
2023-07-26 21:55:53 -0400

    fixed nav bar for media queries

--- script.js
+++ script.js
@@ -18,3 +18,32 @@ document.addEventListener('DOMContentLoaded', function () {
             smoothScroll(target);
         });
 });
+
+// Function to hide social icons when the dropdown is open
+$(document).ready(function () {
+    // Function to toggle social icons based on screen size and dropdown status
+    function toggleSocialIcons() {
+        if ($(window).width() <= 991.98) {
+            // If the screen size is smaller, show social icons in the dropdown
+            $('.hide-on-mobile').show();
+            $('.hide-on-desktop').hide();
+        } else {
+            // If the screen size is larger, show social icons in the navbar
+            $('.hide-on-mobile').hide();
+            $('.hide-on-desktop').show();
+        }
+    }
+
+    // Initially toggle the social icons based on the screen size
+    toggleSocialIcons();
+
+    // When the window is resized, toggle the social icons again
+    $(window).on('resize', function () {
+        toggleSocialIcons();
+    });
+
+    // When the navbar toggler is clicked, toggle the social icons
+    $('.navbar-toggler').on('click', function () {
+        toggleSocialIcons();
+    });
+});
